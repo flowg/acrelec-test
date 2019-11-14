80fa4bf7f9422d15e95302c6e3244af3dbb35283
Florian Gesta <flo.gesta@gmail.com>
2019-11-14 18:08:59 +0100

    Added a first version of the Search functionality

--- src/AppModule/employees-list/employees-list.component.ts
+++ src/AppModule/employees-list/employees-list.component.ts
@@ -15,12 +15,14 @@ import { Employee, EmployeesService } from '../employees.service';
 } )
 export class EmployeesListComponent implements OnInit {
     employees: Employee[];
+    currentEmployees: Employee[];
 
     constructor( private employeesService: EmployeesService ) {
     }
 
     ngOnInit() {
-        this.employees = this.sortByFirstName( this.employeesService.getEmployees() );
+        this.employees        = this.sortByFirstName( this.employeesService.getEmployees() );
+        this.currentEmployees = Array.from( this.employees );
     }
 
     private sortByFirstName( employees: Employee[] ): Employee[] {
@@ -37,5 +39,21 @@ export class EmployeesListComponent implements OnInit {
             // If names are equal
             return 0;
         } );
+    }
+
+    private search( term: string ) {
+        const firstNameFilter: Employee[]  = this.employees.filter( ( employee: Employee ) => {
+            return employee.FirstName.includes( term );
+        } );
+        const secondNameFilter: Employee[] = this.employees.filter( ( employee: Employee ) => {
+            return employee.SecondName.includes( term );
+        } );
+        const positionFilter: Employee[]   = this.employees.filter( ( employee: Employee ) => {
+            return employee.Position.includes( term );
+        } );
+
+        const results: Employee[] = [].concat( firstNameFilter, secondNameFilter, positionFilter );
+
+        this.currentEmployees = term ? results : this.employees;
     }
 }
